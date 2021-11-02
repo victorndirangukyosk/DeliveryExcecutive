fa7f13c8d0a5563c7034d6c8ec4df0d201ff51cf
Baraka <samuel.baraka@technobraingroup.com>
2021-11-02 15:23:53 +0300

    Wrapped the app around a multi bloc provider

--- delivery_app/lib/app/app.dart
+++ delivery_app/lib/app/app.dart
@@ -1,5 +1,7 @@
+import 'package:delivery_app/cubits/obscure_password/obscure_password_cubit.dart';
 import 'package:delivery_app/routes/router.gr.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,18 +12,23 @@ class KwikBasketDeliveryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-        title: "Kwikbasket Delivery App",
-        supportedLocales: const [
-          Locale('en'),
-        ],
-        localizationsDelegates: const [
-          FormBuilderLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
-        routerDelegate: _appRouter.delegate(),
-        routeInformationParser: _appRouter.defaultRouteParser());
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => ObscurePasswordCubit(true))
+      ],
+      child: MaterialApp.router(
+          title: "Kwikbasket Delivery App",
+          supportedLocales: const [
+            Locale('en'),
+          ],
+          localizationsDelegates: const [
+            FormBuilderLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+          routerDelegate: _appRouter.delegate(),
+          routeInformationParser: _appRouter.defaultRouteParser()),
+    );
   }
 }
