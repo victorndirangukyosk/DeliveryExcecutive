6fc9a72be677db47a1a00fe8d707015d86fa8a75
Baraka <samuel.baraka@technobraingroup.com>
2021-11-02 14:54:55 +0300

    New routes config

--- delivery_app/lib/routes/router.dart
+++ delivery_app/lib/routes/router.dart
@@ -1,10 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:delivery_app/user_interfaces/splash_screen/splash_screen.dart';
+import 'package:delivery_app/user_interfaces/user_interfaces.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
   routes: <AutoRoute>[
     AutoRoute(page: SplashScreen, initial: true),
+    AutoRoute(page: LoginPage),
+    AutoRoute(page: RegisterPage),
   ],
 )
 class $AppRouter {}
--- delivery_app/lib/routes/router.gr.dart
+++ delivery_app/lib/routes/router.gr.dart
@@ -8,31 +8,57 @@
 // AutoRouteGenerator
 // **************************************************************************
 
-import 'package:auto_route/auto_route.dart' as _i2;
-import 'package:flutter/material.dart' as _i3;
+import 'package:auto_route/auto_route.dart' as _i3;
+import 'package:flutter/material.dart' as _i4;
 
 import '../user_interfaces/splash_screen/splash_screen.dart' as _i1;
+import '../user_interfaces/user_interfaces.dart' as _i2;
 
-class AppRouter extends _i2.RootStackRouter {
-  AppRouter([_i3.GlobalKey<_i3.NavigatorState>? navigatorKey])
+class AppRouter extends _i3.RootStackRouter {
+  AppRouter([_i4.GlobalKey<_i4.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i2.PageFactory> pagesMap = {
+  final Map<String, _i3.PageFactory> pagesMap = {
     SplashScreen.name: (routeData) {
-      return _i2.MaterialPageX<dynamic>(
+      return _i3.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i1.SplashScreen());
+    },
+    LoginRoute.name: (routeData) {
+      return _i3.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i2.LoginPage());
+    },
+    RegisterRoute.name: (routeData) {
+      return _i3.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i2.RegisterPage());
     }
   };
 
   @override
-  List<_i2.RouteConfig> get routes =>
-      [_i2.RouteConfig(SplashScreen.name, path: '/')];
+  List<_i3.RouteConfig> get routes => [
+        _i3.RouteConfig(SplashScreen.name, path: '/'),
+        _i3.RouteConfig(LoginRoute.name, path: '/login-page'),
+        _i3.RouteConfig(RegisterRoute.name, path: '/register-page')
+      ];
 }
 
 /// generated route for [_i1.SplashScreen]
-class SplashScreen extends _i2.PageRouteInfo<void> {
+class SplashScreen extends _i3.PageRouteInfo<void> {
   const SplashScreen() : super(name, path: '/');
 
   static const String name = 'SplashScreen';
+}
+
+/// generated route for [_i2.LoginPage]
+class LoginRoute extends _i3.PageRouteInfo<void> {
+  const LoginRoute() : super(name, path: '/login-page');
+
+  static const String name = 'LoginRoute';
+}
+
+/// generated route for [_i2.RegisterPage]
+class RegisterRoute extends _i3.PageRouteInfo<void> {
+  const RegisterRoute() : super(name, path: '/register-page');
+
+  static const String name = 'RegisterRoute';
 }
