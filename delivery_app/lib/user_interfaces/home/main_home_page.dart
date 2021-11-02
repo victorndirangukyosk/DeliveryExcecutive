f331584d3d4027572c7db3f55c6a14cb7ed8ab21
Baraka <samuel.baraka@technobraingroup.com>
2021-11-02 15:47:45 +0300

    Added the main home page

---
+++ delivery_app/lib/user_interfaces/home/main_home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainHomePage extends StatelessWidget {
+  const MainHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
