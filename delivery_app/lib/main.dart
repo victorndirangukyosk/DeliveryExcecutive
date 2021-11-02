7b21224b23fc4b223ab81be802b9689eac117ee8
Baraka <samuel.baraka@technobraingroup.com>
2021-11-02 14:26:33 +0300

    Added font licenses

--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:mpesa_flutter_plugin/mpesa_flutter_plugin.dart';
+import 'package:flutter/services.dart';
 
 import 'app/app.dart';
 
 main() async {
-  MpesaFlutterPlugin.setConsumerKey('YpR2aAdj3lKJwwTAd5RPH7JEY33WKM2B');
-  MpesaFlutterPlugin.setConsumerSecret('JawcOXVX88A7XUxj');
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(KwikBasketDeliveryApp());
 }
