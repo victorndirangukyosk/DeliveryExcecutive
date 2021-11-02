24e9bc4955ac524fe0a60bb6501bb992ef4a28ad
Baraka <samuel.baraka@technobraingroup.com>
2021-11-02 14:11:15 +0300

    Added fonts and localizations

---
+++ delivery_app/lib/app/app.dart
@@ -0,0 +1,27 @@
+import 'package:delivery_app/routes/router.gr.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class KwikBasketDeliveryApp extends StatelessWidget {
+  KwikBasketDeliveryApp({Key? key}) : super(key: key);
+  final _appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+        title: "Kwikbasket Delivery App",
+        supportedLocales: const [
+          Locale('en'),
+        ],
+        localizationsDelegates: const [
+          FormBuilderLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+        routerDelegate: _appRouter.delegate(),
+        routeInformationParser: _appRouter.defaultRouteParser());
+  }
+}
