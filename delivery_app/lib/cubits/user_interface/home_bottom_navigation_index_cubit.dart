c0b4f2f0cf72b9bb7f75ed6e2d3133e4ca0b0dfa
Baraka <samuel.baraka@technobraingroup.com>
2021-11-02 15:47:17 +0300

    Added home bottom navigation cubit

---
+++ delivery_app/lib/cubits/user_interface/home_bottom_navigation_index_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeBottomNavigationIndexCubit extends Cubit<int> {
+  HomeBottomNavigationIndexCubit(int initialState) : super(initialState);
+}
