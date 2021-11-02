ecabb96a8b148fba294b00975750ea4bf8d91724
Baraka <samuel.baraka@technobraingroup.com>
2021-11-02 14:04:03 +0300

    Modified Widget test

--- delivery_app/test/widget_test.dart
+++ delivery_app/test/widget_test.dart
@@ -5,26 +5,21 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:delivery_app/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:delivery_app/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App launches successfully', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.pumpWidget(const KwikBasketDeliveryApp());
+    Widget widget = Image.asset(
+      'assets/icon/icon.png',
+      height: 200,
+      width: 200,
+    );
+    Finder x = find.byWidget(widget, skipOffstage: true);
+    // Verify that the quick basket logo appears on the splash screen
+    expect(x, findsOneWidget);
   });
 }
