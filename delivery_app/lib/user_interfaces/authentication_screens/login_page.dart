d6f84855d3d06b3408ed41a5bb412fac1f323b68
Baraka <samuel.baraka@technobraingroup.com>
2021-11-02 15:48:47 +0300

    Replaced import with global import file

--- delivery_app/lib/user_interfaces/authentication_screens/login_page.dart
+++ delivery_app/lib/user_interfaces/authentication_screens/login_page.dart
@@ -1,4 +1,4 @@
-import 'package:delivery_app/cubits/obscure_password/obscure_password_cubit.dart';
+import 'package:delivery_app/cubits/cubits.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
