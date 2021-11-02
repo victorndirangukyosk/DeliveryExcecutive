26417be7d67dbe6662cebd22aec98dce985be793
Baraka <samuel.baraka@technobraingroup.com>
2021-11-02 15:23:31 +0300

    Added obscure password cubit

---
+++ delivery_app/lib/cubits/obscure_password/obscure_password_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ObscurePasswordCubit extends Cubit<bool> {
+  ObscurePasswordCubit(bool initialState) : super(initialState);
+  toggle() {
+    emit(!state);
+  }
+}
