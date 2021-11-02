96d5deff742c53f0ad451e0bb3ffc3d019be257b
Baraka <samuel.baraka@technobraingroup.com>
2021-11-02 15:31:10 +0300

    Added colors

---
+++ delivery_app/lib/configuration/palette/palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class Palette {
+  //The green primary app color
+  static Color greenColor = const Color(0xff2ba349);
+  //The orange accent app color
+  static Color orangeColor = const Color(0xffef7b24);
+}
