6a6bfce51b881b4dae6613059d5f9f894d116f97
Baraka <samuel.baraka@technobraingroup.com>
2021-11-03 13:02:04 +0300

    Added my orders and home tabs

---
+++ delivery_app/lib/user_interfaces/home/my_orders_tab/my_orders_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyOrdersTab extends StatelessWidget {
+  const MyOrdersTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
