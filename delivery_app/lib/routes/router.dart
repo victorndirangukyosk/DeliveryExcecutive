bae7b5df32dc795a0da5f5569e30df406032ebe7
Baraka <samuel.baraka@technobraingroup.com>
2021-11-04 14:05:23 +0300

    Added my orders page

--- delivery_app/lib/routes/router.dart
+++ delivery_app/lib/routes/router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:delivery_app/user_interfaces/home/home_tab/my_orders/my_orders_page.dart';
 import 'package:delivery_app/user_interfaces/home/main_home_page.dart';
 import 'package:delivery_app/user_interfaces/splash_screen/splash_screen.dart';
 import 'package:delivery_app/user_interfaces/user_interfaces.dart';
@@ -10,6 +11,7 @@ import 'package:delivery_app/user_interfaces/user_interfaces.dart';
     AutoRoute(page: LoginPage),
     AutoRoute(page: RegisterPage),
     AutoRoute(page: MainHomePage),
+    AutoRoute(page: MyOrdersPage),
   ],
 )
 class $AppRouter {}
