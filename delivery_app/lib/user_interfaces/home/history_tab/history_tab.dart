aaba656f7ed4e06bf93de07c6db181f825473e21
Baraka <samuel.baraka@technobraingroup.com>
2021-11-04 10:52:52 +0300

    Changed tab pages

--- delivery_app/lib/user_interfaces/home/history_tab/history_tab.dart
+++ delivery_app/lib/user_interfaces/home/history_tab/history_tab.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyOrdersTab extends StatelessWidget {
-  const MyOrdersTab({Key? key}) : super(key: key);
+class HistoryTab extends StatelessWidget {
+  const HistoryTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- delivery_app/lib/user_interfaces/home/main_home_page.dart
+++ delivery_app/lib/user_interfaces/home/main_home_page.dart
@@ -1,24 +1,17 @@
 import 'package:delivery_app/cubits/user_interface/home_bottom_navigation_index_cubit.dart';
+import 'package:delivery_app/user_interfaces/home/home_tab/home_tab.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'history_tab/history_tab.dart';
 
 class MainHomePage extends StatelessWidget {
   const MainHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> widgets = [
-      Container(
-        color: Colors.red,
-      ),
-      Container(
-        color: Colors.blue,
-      ),
-      Container(
-        color: Colors.green,
-      ),
-    ];
+    List<Widget> widgets = const [HomeTab(), HistoryTab()];
 
     return Scaffold(
       body: widgets[context.watch<HomeBottomNavigationIndexCubit>().state],
@@ -29,8 +22,7 @@ class MainHomePage extends StatelessWidget {
           },
           items: const [
             BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
-            BottomNavigationBarItem(icon: Icon(CupertinoIcons.cart)),
-            BottomNavigationBarItem(icon: Icon(CupertinoIcons.building_2_fill))
+            BottomNavigationBarItem(icon: Icon(Icons.history))
           ]),
     );
   }
