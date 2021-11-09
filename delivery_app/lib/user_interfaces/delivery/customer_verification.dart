647b61682a766e9492f052aa65900b54870e053e
Baraka <samuel.baraka@technobraingroup.com>
2021-11-09 11:59:57 +0300

    Added the customer verification screen

---
+++ delivery_app/lib/user_interfaces/delivery/customer_verification.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomerVerification extends StatelessWidget {
+  const CustomerVerification({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
