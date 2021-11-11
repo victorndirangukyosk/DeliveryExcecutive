e9a3f71046f081ed7c4ff7214b7cb12c48dcea80
Baraka <samuel.baraka@technobraingroup.com>
2021-11-11 14:43:03 +0300

    Returned kiosk mode...

--- delivery_app/lib/user_interfaces/splash_screen/splash_screen.dart
+++ delivery_app/lib/user_interfaces/splash_screen/splash_screen.dart
@@ -1,7 +1,10 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:delivery_app/configuration/configuration.dart';
 import 'package:delivery_app/routes/router.gr.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kiosk_mode/kiosk_mode.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -15,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 3), () async {
-      // await startKioskMode();
+      await startKioskMode();
       AutoRouter.of(context).replace(const LoginRoute());
     });
   }
@@ -25,11 +28,32 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
         body: Stack(
       children: [
+        const Positioned(
+          top: 100,
+          right: 0,
+          left: 0,
+          child: Center(
+            child: Text.rich(TextSpan(
+                text: "Kwik ",
+                style: TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.w900,
+                    color: Palette.orangeColor),
+                children: [
+                  TextSpan(
+                      text: "Delivery",
+                      style: TextStyle(color: Palette.greenColor))
+                ])),
+          ),
+        ),
         Positioned(
             left: 0,
             right: 0,
             bottom: -10,
-            child: Image.asset("assets/rider_image.png"))
+            child: SvgPicture.asset(
+              "assets/truck_image.svg",
+              width: MediaQuery.of(context).size.width,
+            ))
       ],
     ));
   }
