9c79aaab31ac6814c8ce4dcca1920b04c3872537
Baraka <samuel.baraka@technobraingroup.com>
2021-11-12 16:27:35 +0300

    Removed unused import

--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'app/app.dart';
 
 ///This is the starting point for the application
