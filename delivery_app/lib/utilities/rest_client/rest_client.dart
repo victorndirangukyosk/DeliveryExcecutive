d2dd0838996170e91d55bce3ea29155cfff28844
Baraka <samuel.baraka@technobraingroup.com>
2021-11-18 11:08:44 +0300

    Implemented the rest client class...

--- delivery_app/lib/utilities/rest_client/rest_client.dart
+++ delivery_app/lib/utilities/rest_client/rest_client.dart
@@ -1,1 +1,31 @@
-class RestClient {}
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class RestClient {
+  Dio? _dio;
+
+  Dio? get dio => _dio;
+
+  String baseURL = '';
+
+  RestClient({BaseOptions? options}) {
+    create(options);
+  }
+
+  void create([BaseOptions? options]) {
+    _dio = Dio(options);
+
+    ///Add the interceptors
+    if (kDebugMode) {
+      _dio!.interceptors.add(PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseBody: true,
+          responseHeader: true,
+          error: true,
+          compact: true,
+          maxWidth: 90));
+    }
+  }
+}
