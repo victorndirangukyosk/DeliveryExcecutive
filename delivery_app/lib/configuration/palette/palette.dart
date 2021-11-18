dc212fb72c011fd38fa53b25368c26bd7a596f90
Baraka <samuel.baraka@technobraingroup.com>
2021-11-18 14:06:45 +0300

    Improved documentation

--- delivery_app/lib/configuration/palette/palette.dart
+++ delivery_app/lib/configuration/palette/palette.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
 class Palette {
-  //The green primary app color
+  /// The green primary app color
   static const Color greenColor = Color(0xff2ba349);
-  //The orange accent app color
+
+  /// The orange accent app color
   static const Color orangeColor = Color(0xffef7b24);
-  //The grey color
+
+  /// The grey color
   static const Color greyColor = Color(0xffF5F5F5);
 }
