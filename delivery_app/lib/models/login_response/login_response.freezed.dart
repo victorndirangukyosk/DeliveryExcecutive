e1db51feeb29f4309ca2b9d332a5cdbce2d74c5e
Baraka <samuel.baraka@technobraingroup.com>
2021-11-19 14:37:56 +0300

    Created a login respnse model class.

---
+++ delivery_app/lib/models/login_response/login_response.freezed.dart
@@ -0,0 +1,109 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'login_response.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+LoginResponse _$LoginResponseFromJson(Map<String, dynamic> json) {
+  return _LoginResponse.fromJson(json);
+}
+
+/// @nodoc
+class _$LoginResponseTearOff {
+  const _$LoginResponseTearOff();
+
+  _LoginResponse call() {
+    return _LoginResponse();
+  }
+
+  LoginResponse fromJson(Map<String, Object> json) {
+    return LoginResponse.fromJson(json);
+  }
+}
+
+/// @nodoc
+const $LoginResponse = _$LoginResponseTearOff();
+
+/// @nodoc
+mixin _$LoginResponse {
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $LoginResponseCopyWith<$Res> {
+  factory $LoginResponseCopyWith(
+          LoginResponse value, $Res Function(LoginResponse) then) =
+      _$LoginResponseCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$LoginResponseCopyWithImpl<$Res>
+    implements $LoginResponseCopyWith<$Res> {
+  _$LoginResponseCopyWithImpl(this._value, this._then);
+
+  final LoginResponse _value;
+  // ignore: unused_field
+  final $Res Function(LoginResponse) _then;
+}
+
+/// @nodoc
+abstract class _$LoginResponseCopyWith<$Res> {
+  factory _$LoginResponseCopyWith(
+          _LoginResponse value, $Res Function(_LoginResponse) then) =
+      __$LoginResponseCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$LoginResponseCopyWithImpl<$Res>
+    extends _$LoginResponseCopyWithImpl<$Res>
+    implements _$LoginResponseCopyWith<$Res> {
+  __$LoginResponseCopyWithImpl(
+      _LoginResponse _value, $Res Function(_LoginResponse) _then)
+      : super(_value, (v) => _then(v as _LoginResponse));
+
+  @override
+  _LoginResponse get _value => super._value as _LoginResponse;
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_LoginResponse implements _LoginResponse {
+  _$_LoginResponse();
+
+  factory _$_LoginResponse.fromJson(Map<String, dynamic> json) =>
+      _$$_LoginResponseFromJson(json);
+
+  @override
+  String toString() {
+    return 'LoginResponse()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _LoginResponse);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_LoginResponseToJson(this);
+  }
+}
+
+abstract class _LoginResponse implements LoginResponse {
+  factory _LoginResponse() = _$_LoginResponse;
+
+  factory _LoginResponse.fromJson(Map<String, dynamic> json) =
+      _$_LoginResponse.fromJson;
+}
---
+++ delivery_app/lib/models/login_response/login_response.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_LoginResponse _$$_LoginResponseFromJson(Map<String, dynamic> json) =>
+    _$_LoginResponse();
+
+Map<String, dynamic> _$$_LoginResponseToJson(_$_LoginResponse instance) =>
+    <String, dynamic>{};
