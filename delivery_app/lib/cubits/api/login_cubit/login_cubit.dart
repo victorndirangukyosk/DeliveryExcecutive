cc567c16c4a549f419f4d899cddc727d64115632
Baraka <samuel.baraka@technobraingroup.com>
2021-11-23 16:45:30 +0300

    Fixed handled errors

--- delivery_app/lib/cubits/api/login_cubit/login_cubit.dart
+++ delivery_app/lib/cubits/api/login_cubit/login_cubit.dart
@@ -2,7 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:delivery_app/models/api_response/api_response.dart';
 import 'package:delivery_app/models/login_response/login_response.dart';
 import 'package:delivery_app/services/api_service/api_service.dart';
-import 'package:dio/dio.dart';
 import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
 
@@ -18,8 +17,8 @@ class LoginCubit extends Cubit<LoginState> {
           await ApiService.post(data: data, path: '/login');
       LoginResponse loginResponse = LoginResponse.fromJson(apiResponse.data!);
       emit(LoginSuccess(loginResponse: loginResponse));
-    } on DioError catch (e) {
-      emit(LoginFailed(error: e.message));
+    } on String catch (e) {
+      emit(LoginFailed(error: e));
     }
   }
 }
