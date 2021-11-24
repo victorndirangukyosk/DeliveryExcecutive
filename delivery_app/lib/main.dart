90d959c8bd3b003fb0a462f4b5fa2f1a9dcb9a94
Baraka <samuel.baraka@technobraingroup.com>
2021-11-24 11:14:10 +0300

    FIxed hydtrated bloc init issue

--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -1,19 +1,25 @@
+import 'dart:io';
+
 import 'package:delivery_app/utilities/utilities.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'app/app.dart';
 
 ///This is the starting point for the application
 main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   //Set the license font the poppins google font
   LicenseRegistry.addLicense(() async* {
     final license = await rootBundle.loadString('google_fonts/OFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
   });
   // await [
-  //   Permission.camera,
+  //   // Permission.camera,
   //   Permission.storage,
   // ].request();
 
@@ -23,5 +29,14 @@ main() async {
   ///Register the rest client to be accessible throughout the app {DI}
   GetIt.I.registerSingleton<RestClient>(RestClient());
 
-  runApp(KwikBasketDeliveryApp());
+  ///Get directory to store all state persists (Securely 😊😊)
+  Directory storageDirectory = await getTemporaryDirectory();
+
+  /// Initialize hydrated storage for all state persists storage
+  final storage =
+      await HydratedStorage.build(storageDirectory: storageDirectory);
+
+  /// Run the app Zoned in [HydratedBloc] overrides
+  HydratedBlocOverrides.runZoned(() => runApp(KwikBasketDeliveryApp()),
+      storage: storage);
 }
