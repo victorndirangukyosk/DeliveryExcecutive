dfad3b0a47ef9a831e229e36b815a488bd88b83d
Baraka <samuel.baraka@technobraingroup.com>
2021-11-24 13:05:38 +0300

    Created global models export file

--- delivery_app/lib/cubits/api/reset_password_cubit/reset_password_cubit.dart
+++ delivery_app/lib/cubits/api/reset_password_cubit/reset_password_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:delivery_app/models/models.dart';
+import 'package:delivery_app/services/services.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 
 part 'reset_password_state.dart';
 
 class ResetPasswordCubit extends Cubit<ResetPasswordState> {
   ResetPasswordCubit() : super(ResetPasswordInitial());
+
+  resetPassword({required Map<String, dynamic> data}) async {
+    emit(ResetPasswordLoading());
+    try {
+      ApiResponse apiResponse = ApiResponse.fromJson(
+          await ApiService.post(data: data, path: '/resetPassword'));
+      LoginResponse loginResponse = LoginResponse.fromJson(apiResponse.data!);
+      emit(ResetPasswordSuccess());
+    } on String catch (e) {
+      emit(ResetPasswordFailed(e));
+    } catch (e) {
+      emit(ResetPasswordFailed('An error has occured'));
+    }
+  }
 }
---
+++ delivery_app/lib/models/models.dart
@@ -0,0 +1,3 @@
+export 'login_response/login_response.dart';
+export 'api_response/api_response.dart';
+export 'reset_password_response/reset_password_response.dart';
