5771d34a99dff4f6167bb3c1a6407e672ca761b4
Baraka <samuel.baraka@technobraingroup.com>
2021-11-24 09:55:31 +0300

    Wrapped the entire app around a Global overlay support widget

--- delivery_app/lib/app/app.dart
+++ delivery_app/lib/app/app.dart
@@ -10,6 +10,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 class KwikBasketDeliveryApp extends StatelessWidget {
   KwikBasketDeliveryApp({Key? key}) : super(key: key);
@@ -18,45 +19,46 @@ class KwikBasketDeliveryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => ObscurePasswordCubit(true)),
-        BlocProvider(create: (context) => HomeBottomNavigationIndexCubit(0)),
-        BlocProvider(create: (context) => SelectDateCubit(DateTime.now())),
-        BlocProvider(create: (context) => PickImageCubit(File(''))),
+        providers: [
+          BlocProvider(create: (context) => ObscurePasswordCubit(true)),
+          BlocProvider(create: (context) => HomeBottomNavigationIndexCubit(0)),
+          BlocProvider(create: (context) => SelectDateCubit(DateTime.now())),
+          BlocProvider(create: (context) => PickImageCubit(File(''))),
 
-        /// This blocprovider persists the token state
-        BlocProvider(create: (context) => TokenCubit('')),
-      ],
-      child: MaterialApp.router(
-          title: 'Kwikbasket Delivery App',
-          debugShowCheckedModeBanner: false,
-          supportedLocales: const [
-            Locale('en'),
-          ],
-          localizationsDelegates: const [
-            FormBuilderLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-          ],
-          theme: ThemeData(
-              textTheme: GoogleFonts.poppinsTextTheme(),
-              primaryColor: Palette.greenColor,
-              colorScheme: const ColorScheme(
-                  primary: Palette.greenColor,
-                  primaryVariant: Colors.black,
-                  secondary: Palette.orangeColor,
-                  secondaryVariant: Colors.amber,
-                  surface: Colors.white,
-                  background: Colors.white,
-                  error: Colors.red,
-                  onPrimary: Colors.white,
-                  onSecondary: Colors.white,
-                  onSurface: Palette.greenColor,
-                  onBackground: Palette.greenColor,
-                  onError: Colors.white,
-                  brightness: Brightness.light)),
-          routerDelegate: _appRouter.delegate(),
-          routeInformationParser: _appRouter.defaultRouteParser()),
-    );
+          /// This blocprovider persists the token state
+          BlocProvider(create: (context) => TokenCubit('')),
+        ],
+        child: OverlaySupport.global(
+          child: MaterialApp.router(
+              title: 'Kwikbasket Delivery App',
+              debugShowCheckedModeBanner: false,
+              supportedLocales: const [
+                Locale('en'),
+              ],
+              localizationsDelegates: const [
+                FormBuilderLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+              ],
+              theme: ThemeData(
+                  textTheme: GoogleFonts.poppinsTextTheme(),
+                  primaryColor: Palette.greenColor,
+                  colorScheme: const ColorScheme(
+                      primary: Palette.greenColor,
+                      primaryVariant: Colors.black,
+                      secondary: Palette.orangeColor,
+                      secondaryVariant: Colors.amber,
+                      surface: Colors.white,
+                      background: Colors.white,
+                      error: Colors.red,
+                      onPrimary: Colors.white,
+                      onSecondary: Colors.white,
+                      onSurface: Palette.greenColor,
+                      onBackground: Palette.greenColor,
+                      onError: Colors.white,
+                      brightness: Brightness.light)),
+              routerDelegate: _appRouter.delegate(),
+              routeInformationParser: _appRouter.defaultRouteParser()),
+        ));
   }
 }
