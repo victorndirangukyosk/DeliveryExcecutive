1bbdc5056570c920ee46309bd38d26536ea6d06e
Baraka <samuel.baraka@technobraingroup.com>
2021-11-24 12:16:51 +0300

    Added reset password state managers

---
+++ delivery_app/lib/cubits/api/reset_password_cubit/reset_password_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'reset_password_state.dart';
+
+class ResetPasswordCubit extends Cubit<ResetPasswordState> {
+  ResetPasswordCubit() : super(ResetPasswordInitial());
+}
---
+++ delivery_app/lib/cubits/api/reset_password_cubit/reset_password_state.dart
@@ -0,0 +1,26 @@
+part of 'reset_password_cubit.dart';
+
+abstract class ResetPasswordState extends Equatable {}
+
+class ResetPasswordInitial extends ResetPasswordState {
+  @override
+  List<Object?> get props => [];
+}
+
+class ResetPasswordLoading extends ResetPasswordState {
+  @override
+  List<Object?> get props => [];
+}
+
+class ResetPasswordSuccess extends ResetPasswordState {
+  @override
+  List<Object?> get props => [];
+}
+
+class ResetPasswordFailed extends ResetPasswordState {
+  final String error;
+
+  ResetPasswordFailed(this.error);
+  @override
+  List<Object?> get props => [error];
+}
