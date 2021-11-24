7993c58a0a7cd31b277493c18604b3f379525bc5
Baraka <samuel.baraka@technobraingroup.com>
2021-11-24 11:13:30 +0300

    Finished login integration to the login page

--- delivery_app/lib/user_interfaces/authentication_screens/login_page.dart
+++ delivery_app/lib/user_interfaces/authentication_screens/login_page.dart
@@ -3,9 +3,12 @@ import 'package:delivery_app/configuration/configuration.dart';
 import 'package:delivery_app/configuration/palette/palette.dart';
 import 'package:delivery_app/cubits/cubits.dart';
 import 'package:delivery_app/routes/router.gr.dart';
+import 'package:delivery_app/utilities/toast/toast.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -34,6 +37,7 @@ class LoginCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var _formKey = GlobalKey<FormBuilderState>();
     return Card(
       elevation: 20,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
@@ -42,80 +46,113 @@ class LoginCard extends StatelessWidget {
         padding: const EdgeInsets.only(left: 20.0, right: 20.0),
         child: SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              const Text.rich(TextSpan(
-                  text: 'Kwik ',
-                  style: TextStyle(
-                      fontSize: 60,
-                      fontWeight: FontWeight.w900,
-                      color: Palette.orangeColor),
-                  children: [
-                    TextSpan(
-                        text: 'Delivery',
-                        style: TextStyle(color: Palette.greenColor))
-                  ])),
-              const SizedBox(
-                height: 40,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(width: 2)),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(width: 2)),
+          child: FormBuilder(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                obscureText: context.watch<ObscurePasswordCubit>().state,
-                decoration: InputDecoration(
-                  suffixIcon: CupertinoButton(
-                      padding: const EdgeInsets.all(0.0),
-                      child: Icon(!context.watch<ObscurePasswordCubit>().state
-                          ? CupertinoIcons.lock_open
-                          : CupertinoIcons.lock),
-                      onPressed: () {
-                        context.read<ObscurePasswordCubit>().toggle();
-                      }),
-                  hintText: 'Password',
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(width: 2)),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(width: 2)),
+                const Text.rich(TextSpan(
+                    text: 'Kwik ',
+                    style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.w900,
+                        color: Palette.orangeColor),
+                    children: [
+                      TextSpan(
+                          text: 'Delivery',
+                          style: TextStyle(color: Palette.greenColor))
+                    ])),
+                const SizedBox(
+                  height: 40,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CupertinoButton(
-                  child: const Text('Login'),
-                  color: Palette.greenColor,
-                  onPressed: () {
-                    AutoRouter.of(context).push(const MainHomeRoute());
-                  }),
-              const SizedBox(
-                height: 20,
-              ),
-              CupertinoButton(
-                  child: const Text('Forgot password?'), onPressed: () {}),
-              const SizedBox(
-                height: 20,
-              ),
-            ],
+                FormBuilderTextField(
+                  name: 'email',
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(context),
+                    FormBuilderValidators.email(context),
+                  ]),
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(width: 2)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(width: 2)),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FormBuilderTextField(
+                  name: 'password',
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(context),
+                  ]),
+                  obscureText: context.watch<ObscurePasswordCubit>().state,
+                  decoration: InputDecoration(
+                    suffixIcon: CupertinoButton(
+                        padding: const EdgeInsets.all(0.0),
+                        child: Icon(!context.watch<ObscurePasswordCubit>().state
+                            ? CupertinoIcons.lock_open
+                            : CupertinoIcons.lock),
+                        onPressed: () {
+                          context.read<ObscurePasswordCubit>().toggle();
+                        }),
+                    hintText: 'Password',
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(width: 2)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(width: 2)),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                BlocConsumer<LoginCubit, LoginState>(
+                  listener: (context, state) async {
+                    if (state is LoginSuccess) {
+                      await AppToast.showToast(
+                          message: 'Login successful', isError: false);
+                      context
+                          .read<TokenCubit>()
+                          .saveToken(state.loginResponse.token!);
+                      AutoRouter.of(context).replace(const MainHomeRoute());
+                    }
+                    if (state is LoginFailed) {
+                      AppToast.showToast(message: state.error, isError: true);
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is LoginLoading) {
+                      return const CupertinoActivityIndicator();
+                    }
+                    return CupertinoButton(
+                        child: const Text('Login'),
+                        color: Palette.greenColor,
+                        onPressed: () {
+                          if (_formKey.currentState!.saveAndValidate()) {
+                            context
+                                .read<LoginCubit>()
+                                .login(data: _formKey.currentState!.value);
+                          }
+                        });
+                  },
+                ),
+                CupertinoButton(
+                    child: const Text('Forgot password?'), onPressed: () {}),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
           ),
         ),
       ),
