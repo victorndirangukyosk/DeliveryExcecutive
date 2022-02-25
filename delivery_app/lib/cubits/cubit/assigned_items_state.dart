5c6cc99c0f775885940dcaa0258b25ca64a088f0
Victor Kwikbasket <victor.ndirangu@technobraingroup.com>
2022-02-25 09:57:16 +0300

    APIs

---
+++ delivery_app/lib/cubits/cubit/assigned_items_state.dart
@@ -0,0 +1,10 @@
+part of 'assigned_items_cubit.dart';
+
+abstract class AssignedItemsState extends Equatable {
+  const AssignedItemsState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AssignedItemsInitial extends AssignedItemsState {}
