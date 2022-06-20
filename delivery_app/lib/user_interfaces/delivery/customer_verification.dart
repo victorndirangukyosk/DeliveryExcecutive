a5088cf357fd4a7dac5b8ab4d7c42f508de9afd6
Victor Kwikbasket <96406967+bigSurs@users.noreply.github.com>
2022-06-20 13:43:41 +0300

    verification cubit placement changes

--- delivery_app/lib/user_interfaces/delivery/customer_verification.dart
+++ delivery_app/lib/user_interfaces/delivery/customer_verification.dart
@@ -14,9 +14,14 @@ import 'package:image_picker/image_picker.dart';
 import 'package:signature/signature.dart';
 import 'package:user_profile_avatar/user_profile_avatar.dart';
 
-class CustomerVerification extends StatelessWidget {
+class CustomerVerification extends StatefulWidget {
   const CustomerVerification({Key? key}) : super(key: key);
 
+  @override
+  State<CustomerVerification> createState() => _CustomerVerificationState();
+}
+
+class _CustomerVerificationState extends State<CustomerVerification> {
   @override
   Widget build(BuildContext context) {
     // context.read<CustomerVerificationCubit>().verifyCustomer(orderId: orderId, File file);
@@ -65,107 +70,110 @@ class CustomerVerification extends StatelessWidget {
         builder: (context, state) {
           return Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  'Customer photo',
-                  style: TextStyle(
-                      color: Palette.greenColor,
-                      fontSize: 20,
-                      fontFamily: 'Helvetica',
-                      fontWeight: FontWeight.bold),
-                ),
-                context.watch<PickImageCubit>().state.path.length < 2
-                    ? InkWell(
-                        onTap: () async {
-                          var x = await ImagePicker()
-                              .pickImage(source: ImageSource.camera);
-                          context.read<PickImageCubit>().emit(File(x!.path));
-                        },
-                        child: AnimatedContainer(
-                          duration: const Duration(seconds: 3),
-                          height: 200,
-                          width: 200,
-                          decoration:
-                              BoxDecoration(border: Border.all(width: 2)),
-                          child: Center(
-                            child: UserProfileAvatar(
-                              avatarUrl: '',
-                              onAvatarTap: () async {
-                                ScaffoldMessenger.of(context)
-                                    .showSnackBar(SnackBar(
-                                  content: Text('Tapped on avatar'),
-                                ));
-                                var x = await ImagePicker()
-                                    .pickImage(source: ImageSource.camera);
-                                context
-                                    .read<PickImageCubit>()
-                                    .emit(File(x!.path));
-                              },
-                              // notificationCount: 10,
-                              notificationBubbleTextStyle: TextStyle(
-                                fontSize: 30,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              avatarSplashColor: Colors.purple,
-                              radius: 100,
-                              isActivityIndicatorSmall: false,
-                              avatarBorderData: AvatarBorderData(
-                                borderColor: Colors.black54,
-                                borderWidth: 5.0,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Customer photo',
+                    style: TextStyle(
+                        color: Palette.greenColor,
+                        fontSize: 20,
+                        fontFamily: 'Helvetica',
+                        fontWeight: FontWeight.bold),
+                  ),
+                  context.watch<PickImageCubit>().state.path.length < 2
+                      ? InkWell(
+                          onTap: () async {
+                            var x = await ImagePicker()
+                                .pickImage(source: ImageSource.camera);
+                            context.read<PickImageCubit>().emit(File(x!.path));
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(seconds: 3),
+                            height: 200,
+                            width: 200,
+                            decoration:
+                                BoxDecoration(border: Border.all(width: 2)),
+                            child: Center(
+                              child: UserProfileAvatar(
+                                avatarUrl: '',
+                                onAvatarTap: () async {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text('Tapped on avatar'),
+                                  ));
+                                  var x = await ImagePicker()
+                                      .pickImage(source: ImageSource.camera);
+                                  context
+                                      .read<PickImageCubit>()
+                                      .emit(File(x!.path));
+                                },
+                                // notificationCount: 10,
+                                notificationBubbleTextStyle: TextStyle(
+                                  fontSize: 30,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                avatarSplashColor: Colors.purple,
+                                radius: 100,
+                                isActivityIndicatorSmall: false,
+                                avatarBorderData: AvatarBorderData(
+                                  borderColor: Colors.black54,
+                                  borderWidth: 5.0,
+                                ),
                               ),
                             ),
                           ),
+                        )
+                      : Image.file(
+                          context.read<PickImageCubit>().state,
+                          height: 200,
+                          width: 300,
+                          fit: BoxFit.cover,
                         ),
-                      )
-                    : Image.file(
-                        context.read<PickImageCubit>().state,
-                        height: 200,
-                        width: 300,
-                        fit: BoxFit.cover,
-                      ),
-                const SizedBox(
-                  height: 70,
-                ),
-                const Text(
-                  'Customer signature',
-                  style: TextStyle(
-                      color: Palette.greenColor,
-                      fontSize: 20,
-                      fontFamily: 'Helvetica',
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                ),
-                Container(
-                  height: 200,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color: Palette.greenColor,
-                    border: Border.all(width: 1),
-                    // borderRadius: BorderRadius.circular(30),
+                  const SizedBox(
+                    height: 70,
                   ),
-                  child: Signature(
-                    controller: SignatureController(),
+                  const Text(
+                    'Customer signature',
+                    style: TextStyle(
+                        color: Palette.greenColor,
+                        fontSize: 20,
+                        fontFamily: 'Helvetica',
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                  Container(
                     height: 200,
                     width: 300,
-                    backgroundColor: Colors.white,
+                    decoration: BoxDecoration(
+                      color: Palette.greenColor,
+                      border: Border.all(width: 1),
+                      // borderRadius: BorderRadius.circular(30),
+                    ),
+                    //we can have the following cubits analyzed
+                    child: Signature(
+                      controller: SignatureController(),
+                      height: 200,
+                      width: 300,
+                      backgroundColor: Colors.white,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                CupertinoButton(
-                    child: const Text('Submit'),
-                    color: Palette.greenColor,
-                    onPressed: () {
-                      context.read<CustomerVerificationCubit>();
-                    })
-              ],
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  CupertinoButton(
+                      child: const Text('Submit'),
+                      color: Palette.greenColor,
+                      onPressed: () {
+                        context.read<CustomerVerificationCubit>();
+                      })
+                ],
+              ),
             ),
           );
         },
